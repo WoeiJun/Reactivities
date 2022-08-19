3470b603f45d98608701609e6cedc6a45f70340e
WoeiJun <woeijong@aemulus.com>
2022-08-19 16:03:36 +0800

    End of Section 10

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'react-calendar/dist/Calendar.css';
+import 'react-toastify/dist/ReactToastify.min.css';
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
 import { BrowserRouter } from 'react-router-dom';
 
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StoreContext.Provider value={store}>
-    <BrowserRouter>
+    <BrowserRouter >
     <App />
     </BrowserRouter>
   </StoreContext.Provider>
