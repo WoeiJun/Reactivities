bff42b2d397f3841fc5f4cef1db1027e3a30abe6
WoeiJun <woeijong@aemulus.com>
2022-08-29 16:10:10 +0800

    End of Section 13

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -9,13 +9,30 @@ import ActivityDetails from '../../features/activities/details/ActivityDetail';
 import TestErrors from '../../features/errors/TestError';
 import { ToastContainer } from 'react-toastify';
 import NotFound from '../../features/errors/NotFound';
+import LoginForm from '../../features/users/LoginForm';
+import { useStore } from '../stores/store';
+import { useEffect } from 'react';
+import LoadingComponent from './LoadingComponent';
+import ModalContainer from '../common/modals/ModalContainer';
 
 function App() {
   const location = useLocation();
+  const {commonStore, userStore} = useStore();
+
+  useEffect(() => {
+    if (commonStore.token){
+      userStore.getUser().finally(() => commonStore.setAppLoaded());
+    } else{
+      commonStore.setAppLoaded();
+    }
+  }, [commonStore, userStore])
+
+  if(!commonStore.appLoaded) return <LoadingComponent content='Loading app...' />
 
   return (
     <>
       <ToastContainer position='bottom-right' hideProgressBar />
+      <ModalContainer />
       <Route exact path='/' component={HomePage} />
       <Route
         path={'/(.+)'}
@@ -28,6 +45,7 @@ function App() {
               <Route path='/activities/:id' component={ActivityDetails} />
               <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
               <Route path='/errors' component={TestErrors} />
+              <Route path='/login' component ={LoginForm} />
               <Route component={NotFound} />
               </Switch>
             </Container>
